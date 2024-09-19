0411c3bcedfc4730cca89e705894c14eef85ca7f
NISHINOKIRI <v.garanin@gost.team>
2024-09-19 22:18:09 +0300

    Add files via upload

---
+++ Выведите символ.py
@@ -0,0 +1,6 @@
+char = input('Введите символ: ')
+print (char)
+print (char + char)
+print (char + char + char)
+print (char + char + char + char)
+print (char + char + char + char + char)
---
+++ Номер месяца.py
@@ -0,0 +1,27 @@
+month_number = input('Введите номер месяца: ')
+if month_number == '1':
+    print ('Январь')
+if month_number == '2':
+    print ('Февраль')
+if month_number == '3':
+    print ('Март')
+if month_number == '4':
+    print ('Апрель')   
+if month_number == '5':
+    print ('Май')
+if month_number == '6':
+    print ('Июнь')
+if month_number == '7':
+    print ('Июль')
+if month_number == '8':
+    print ('Август')   
+if month_number == '9':
+    print ('Сентябрь')
+if month_number == '10':
+    print ('Октябрь')
+if month_number == '11':
+    print ('Ноябрь')
+if month_number == '12':
+    print ('Декабрь')
+else:
+    print ('Такого месяца не существует!', 'Скрипт завершён', '\n')
