f1126d7aa537233b7e54a91912988ecf929bc859
NISHINOKIRI <v.garanin@gost.team>
2024-09-20 10:29:27 +0300

    Update Номер месяца.py

--- Номер месяца.py
+++ Номер месяца.py
@@ -1,27 +1,27 @@
 month_number = input('Введите номер месяца: ')
 if month_number == '1':
     print ('Январь')
-if month_number == '2':
+elif month_number == '2':
     print ('Февраль')
-if month_number == '3':
+elif month_number == '3':
     print ('Март')
-if month_number == '4':
+elif month_number == '4':
     print ('Апрель')   
-if month_number == '5':
+elif month_number == '5':
     print ('Май')
-if month_number == '6':
+elif month_number == '6':
     print ('Июнь')
-if month_number == '7':
+elif month_number == '7':
     print ('Июль')
-if month_number == '8':
+elif month_number == '8':
     print ('Август')   
-if month_number == '9':
+elif month_number == '9':
     print ('Сентябрь')
-if month_number == '10':
+elif month_number == '10':
     print ('Октябрь')
-if month_number == '11':
+elif month_number == '11':
     print ('Ноябрь')
-if month_number == '12':
+elif month_number == '12':
     print ('Декабрь')
 else:
     print ('Такого месяца не существует!', 'Скрипт завершён', '\n')
