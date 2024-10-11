2a1db8befd0ba3158a3b6dcf924ec771015cd769
NISHINOKIRI <v.garanin@gost.team>
2024-10-11 17:44:55 +0300

    Add files via upload

---
+++ Home Work 5/Список слов, сплит по пробелам, декоратор в верхинй регистр.py
@@ -0,0 +1,25 @@
+def get_split_words_list():
+    words_list = input('Enter some words (separated by spaces) :\n').split()
+    return words_list
+
+def shown_word_list(words_list):
+    print('\nWords list:')
+    print(words_list)
+
+def dec_up_reg(func):
+    def zalupper(words):
+        result = func(words)
+        return [word.upper() for word in result]
+    return zalupper
+
+@dec_up_reg
+def show_words(words):
+    return words
+
+def print_result():
+    words = get_split_words_list()
+    shown_word_list(words)
+    upper_words = show_words(words)
+    print('\nUppercase word:\n' + ', '.join(f'"{word}"' for word in upper_words) + ';')
+
+print_result()
