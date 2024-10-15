55a63033599233541ed9f8ead0071c2673fa6979
NISHINOKIRI <v.garanin@gost.team>
2024-10-15 17:53:43 +0300

    Update main.py

--- home_work_6/basics/main.py
+++ home_work_6/basics/main.py
@@ -1,39 +1,25 @@
 import datetime
 from lib.client.client import App
 
-def task_manager():
+def manage_tasks_and_achievements():
+    # Сохранение задач
     with open('task_list.txt', 'w', encoding='utf8') as file:
         file.write(app.get_tasks())
 
-def ach_manager():
+    # Сохранение ачивок
     with open('achivments_list.txt', 'w', encoding='utf8') as file:
         for achievement in app.get_achievements():
             file.write(f'Ачивка: "{achievement.name}"\nДата получения: {achievement.receipt_date}; \nУсловия выдачи: "{achievement.description}";\n\n')
 
+    # Чтение задач
+    with open('task_list.txt', 'r', encoding='utf8') as file:
+        print("\nСписок задач:\n")
+        print(file.read())
 
-def read_tasks():
-    try:
-        with open('task_list.txt', 'r', encoding='utf8') as file:
-            tasks = file.read()
-            print()
-            print(tasks)
-    except FileNotFoundError:
-        print("Файл с задачами не найден.")
-    except Exception as e:
-        print(f"Произошла ошибка при чтении файла задач: {e}")
-
-def read_achievements():
-    try:
-        with open('achivments_list.txt', 'r', encoding='utf8') as file:
-            achievements = file.read()
-            print("Список ачивок:\n")
-            print(achievements)
-    except FileNotFoundError:
-        print("Файл с ачивками не найден.")
-    except Exception as e:
-        print(f"Произошла ошибка при чтении файла ачивок: {e}")
-
-
+    # Чтение ачивок
+    with open('achivments_list.txt', 'r', encoding='utf8') as file:
+        print("Список ачивок:\n")
+        print(file.read())
 
 app = App(
     username='Nishinokiri',
@@ -118,7 +104,9 @@ print(app.remove_task(6))
 # print(app.get_tasks())
 # app.get_and_print_achievements()
 
-ach_manager() # Записываем ачивки в файл
-task_manager() # Записываем задачи в файл (из рефакторенного _get_tasks)
-read_tasks()
-read_achievements()
+# ach_manager() # Записываем ачивки в файл
+# task_manager() # Записываем задачи в файл (из рефакторенного _get_tasks)
+# read_tasks()
+# read_achievements()
+
+manage_tasks_and_achievements()
