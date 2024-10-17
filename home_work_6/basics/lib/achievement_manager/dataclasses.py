610c5017ce86c549e763ada7e8e912b20d2aaa08
NISHINOKIRI <v.garanin@gost.team>
2024-10-17 18:29:43 +0300

    Delete home_work_6/basics directory

--- home_work_6/basics/lib/achievement_manager/dataclasses.py
+++
@@ -1,9 +0,0 @@
-import dataclasses
-import datetime
-
-
-@dataclasses.dataclass
-class Achievement:
-    name: str
-    description: str
-    receipt_date: datetime.date = datetime.date.today().strftime('%Y-%m-%d')
