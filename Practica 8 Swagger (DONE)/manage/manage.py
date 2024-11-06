0c3ff8c92a5f2bc14647939ac6ef4299b26d033d
NISHINOKIRI <v.garanin@gost.team>
2024-11-06 14:59:16 +0300

    Update manage.py

--- Practica 8 Swagger (DONE)/manage/manage.py
+++ Practica 8 Swagger (DONE)/manage/manage.py
@@ -5,7 +5,6 @@ import requests
 class Manage:
     @staticmethod
     def request_info():
-        # Пример использования класса
         swagger_pet = SwaggerPetID()  # Создаём экземпляр
         pet_id = swagger_pet.get_id_from_user()  # Получаем ID питомца
         swagger_pet.request_processing(pet_id)  # Получаем данные о питомце (если они есть в обработанном ответе)
@@ -19,25 +18,6 @@ class Manage:
         pet_id = swagger_pet.get_id_from_user()
         # Удаляем питомца
         response_data = swagger_pet.dell(pet_id)  # Выполняем удаление питомца
-        # Проверяем, успешно ли прошло удаление
-
-    # @staticmethod
-    # def add():
-    #     swagger_pet = SwaggerPetID()
-    #     # Получаем данные о питомце от пользователя
-    #     while True:
-    #         pet_id = input("\nВведите ID для нового питомца: ")
-    #         if id is None or id <= 0:
-    #             raise ValueError("ID питомца должен быть больше 0 и не может быть пустым.")
-    #         pet_name = input("Введите имя питомца: ")
-    #         category_name = input("Введите категорию питомца: ")
-    #         pet_status = input("Введите статус питомца (доступен/нет): ")
-    #         # Вызываем метод добавления питомца
-    #         response_data = swagger_pet.add_pet(id=pet_id, name=pet_name, category_name=category_name, status=pet_status)
-    #         if response_data is not None:
-    #             print("Питомец успешно добавлен.")
-    #         else:
-    #             print(f"Не удалось добавить питомца с ID {pet_id}.")
 
     @staticmethod
     def add():
